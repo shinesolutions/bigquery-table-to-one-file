90b759d0924ca08963edba3f434854085d95d90a
Graham Polley <graham.polley@shinesolutions.com>
2017-09-28 16:11:18 +1000

    First version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bigquery-table-to-one-file
-Using Cloud Dataflow, read a table in BigQuery, and turns it into one file in GCS (BigQuery only support sharded exports over 1GB).
+Using Cloud Dataflow, this Java app reads a table in BigQuery, and turns it into one file in GCS (BigQuery only support sharded exports over 1GB).
+It Uses the default credentials set to the environment variable `GOOGLE_APPLICATION_CREDENTIALS`
+In the code, change the table name and bucket details etc. to suit your needs. You will need to create the GCS buckets yourself.
+
+To run:
+
+`--project=<your_project_id>
+--runner=DataflowRunner
+--jobName=bigquery-table-to-one-file
+--maxNumWorkers=50
+--zone=australia-southeast1-a
+--stagingLocation=gs://<your_bucket>/jars
+--tempLocation=gs://<your_bucket>/tmp`
---
+++ src/main/java/com/shinesolutions/BigQueryTableToOneFile.java
@@ -0,0 +1,39 @@
+package com.shinesolutions;
+
+import com.google.api.services.bigquery.model.TableRow;
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+
+import static org.apache.beam.runners.dataflow.options.DataflowPipelineWorkerPoolOptions.AutoscalingAlgorithmType.THROUGHPUT_BASED;
+
+/**
+ * BigQuery -> ParDo -> GCS (one file)
+ */
+public class BigQueryTableToOneFile {
+    private static final String BIGQUERY_TABLE = "bigquery-samples:wikipedia_benchmark.Wiki1B";
+    private static final String GCS_OUTPUT_FILE = "gs://bigquery-table-to-one-file/output/result.csv";
+
+    public static void main(String[] args) throws Exception {
+        DataflowPipelineOptions options = PipelineOptionsFactory
+                .fromArgs(args)
+                .withValidation()
+                .as(DataflowPipelineOptions.class);
+        options.setAutoscalingAlgorithm(THROUGHPUT_BASED);
+        Pipeline pipeline = Pipeline.create(options);
+        pipeline.apply(BigQueryIO.read().from(BIGQUERY_TABLE))
+                .apply(ParDo.of(new DoFn<TableRow, String>() {
+                    @ProcessElement
+                    public void processElement(ProcessContext c) throws Exception {
+                        TableRow row = c.element();
+                        c.output(row.toPrettyString()); //e.g. {year=2010, month=1, day=1, wikimedia_project=m, language=commons, title=Image:Coat_of_arms_kautenbach_luxbrg.png, views=1}
+                    }
+                }))
+                .apply(TextIO.write().to(GCS_OUTPUT_FILE).withoutSharding());
+        pipeline.run();
+    }
+}
