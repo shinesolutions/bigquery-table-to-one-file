be099822461c9bb5d4e00d413046669af0764226
Graham Polley <graham.polley@shinesolutions.com>
2017-10-23 19:55:31 +1100

    now runs as a templated pipeline and no need to specify the schema

--- src/main/java/com/shinesolutions/BigQueryTableToOneFile.java
+++ src/main/java/com/shinesolutions/BigQueryTableToOneFile.java
@@ -5,11 +5,12 @@ import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.apache.beam.runners.dataflow.options.DataflowPipelineWorkerPoolOptions.AutoscalingAlgorithmType.THROUGHPUT_BASED;
@@ -19,32 +20,41 @@ import static org.apache.beam.sdk.io.FileBasedSink.CompressionType.GZIP;
  * BigQuery -> ParDo -> GCS (one file)
  */
 public class BigQueryTableToOneFile {
-    private static final String BIGQUERY_TABLE = "bigquery-samples:wikipedia_benchmark.Wiki1M";
-    private static final String GCS_OUTPUT_FILE = "gs://bigquery-table-to-one-file/output/wiki_1M.csv";
-    private static final String[] TABLE_ROW_FIELDS = {"year", "month", "day", "wikimedia_project", "language", "title", "views"};
-
     public static void main(String[] args) throws Exception {
-        DataflowPipelineOptions options = PipelineOptionsFactory
+        PipelineOptionsFactory.register(TemplateOptions.class);
+        TemplateOptions options = PipelineOptionsFactory
                 .fromArgs(args)
                 .withValidation()
-                .as(DataflowPipelineOptions.class);
+                .as(TemplateOptions.class);
         options.setAutoscalingAlgorithm(THROUGHPUT_BASED);
         Pipeline pipeline = Pipeline.create(options);
-        pipeline.apply(BigQueryIO.read().from(BIGQUERY_TABLE))
+        pipeline.apply(BigQueryIO.read().from(options.getBigQueryTableName()))
                 .apply(ParDo.of(new DoFn<TableRow, String>() {
                     @ProcessElement
                     public void processElement(ProcessContext c) throws Exception {
-                        String commaSep = Arrays.asList(TABLE_ROW_FIELDS)
+                        String commaSep = c.element().values()
                                 .stream()
-                                .map(field -> c.element().get(field).toString().replaceAll(",", "-"))
-                                .collect(Collectors.joining(","));
+                                .map(cell -> cell.toString().trim())
+                                .collect(Collectors.joining("\",\""));
                         c.output(commaSep);
                     }
                 }))
-                .apply(TextIO.write().to(GCS_OUTPUT_FILE)
+                .apply(TextIO.write().to(options.getOutputFile())
                         .withoutSharding()
                         .withWritableByteChannelFactory(GZIP)
                 );
         pipeline.run();
+    }
+
+    public interface TemplateOptions extends DataflowPipelineOptions {
+        @Description("The BigQuery table to read from in the format project:dataset.table")
+        ValueProvider<String> getBigQueryTableName();
+
+        void setBigQueryTableName(ValueProvider<String> value);
+
+        @Description("The name of the output file to produce in the format gs://bucket_name/filname.csv")
+        ValueProvider<String> getOutputFile();
+
+        void setOutputFile(ValueProvider<String> value);
     }
 }
